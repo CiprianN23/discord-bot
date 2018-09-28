9d94fee249f09761e73daad9cd81f6179d45a0b2
Neagoe Ciprian <neagoeciprian16@yahoo.com>
2018-09-28 19:29:12 +0300

    Update Program.cs
    Changed "hello world" to "hello, Discord"

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -6,7 +6,7 @@ namespace DiscordBot
     {
         private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, Discord!");
         }
     }
 }
