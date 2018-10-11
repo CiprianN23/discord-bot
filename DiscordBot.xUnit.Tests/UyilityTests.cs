8009d7c073446f691a1ece0fbeddd2b899552f57
Neagoe Ciprian <neagoeciprian16@yahoo.com>
2018-10-11 18:28:46 +0300

    Add Unit Tests

---
+++ DiscordBot.xUnit.Tests/UyilityTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace DiscordBot.xUnit.Tests
+{
+    public class UyilityTests
+    {
+        [Fact]
+        public void MyFirstTest()
+        {
+            const int expected = 5;
+
+            var actual = Utilities.MyUtility(expected);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+    
+}
---
+++ DiscordBot/Utilities.cs
@@ -0,0 +1,10 @@
+﻿namespace DiscordBot
+{
+    public class Utilities
+    {
+        public static int MyUtility(int i)
+        {
+            return i;
+        }
+    }
+}
